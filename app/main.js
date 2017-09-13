999af4d6c18bcfc4d290922995312fc729be4177
dmitryr <dmitryr@boaelite.com>
2017-09-13 18:08:31 +0300

    Init

--- app/main.js
+++ app/main.js
@@ -2,7 +2,12 @@ System.register(["angular2/platform/browser", "./app.component"], function(expor
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var browser_1, app_component_1;
-    var mainObj, CurrentMainClass;
+    var mainObj, CurrentMainClass, names, people, myFunc2, GlobalTestClass, globclass, Shapes, rect2;
+    //-------------------------------------------------------------------------------------------
+    function getArrayLength(x) {
+        var len = x[0].length;
+        return len;
+    }
     return {
         setters:[
             function (browser_1_1) {
@@ -17,9 +22,43 @@ System.register(["angular2/platform/browser", "./app.component"], function(expor
             mainObj.showString();
             CurrentMainClass = (function () {
                 function CurrentMainClass() {
+                    this.currentString = 'CURR STRING';
+                    this.currentNumber = 999;
+                    console.log('currentString:', this.currentString, ' ::: currentNumber:', this.currentNumber);
                 }
                 return CurrentMainClass;
             }());
+            this.curr_main = new CurrentMainClass();
+            names = ['John', 'Dan', 'Aaron', 'Fritz'];
+            console.log(getArrayLength(names));
+            people = [{ name: 'Aaron' }, { name: 'Fritz' }];
+            console.log(getArrayLength(people)); //--------------------return undefined because array of objects (nor string)
+            myFunc2 = function (h, w) { return h * w; };
+            console.log('LAMBDA FUNCTION (3*5):', myFunc2(3, 5));
+            //--------------------------------------------------------------------------------
+            GlobalTestClass = (function () {
+                function GlobalTestClass() {
+                    this.a = 'Private a';
+                    this.b = 'Public b';
+                }
+                return GlobalTestClass;
+            }());
+            globclass = new GlobalTestClass();
+            console.log('GLOBAL TEST CLASS INSTANCE:', globclass);
+            (function (Shapes) {
+                var Rectangle = (function () {
+                    //private privateVar = 'privateVar';
+                    function Rectangle(height, width) {
+                        this.height = height;
+                        this.width = width;
+                    }
+                    return Rectangle;
+                }());
+                Shapes.Rectangle = Rectangle;
+            })(Shapes || (Shapes = {}));
+            console.log('SHAPES 1:', Shapes);
+            rect2 = Shapes.Rectangle(10, 4);
+            console.log('SHAPES 2:', rect2);
         }
     }
 });
--- app/main.ts
+++ app/main.ts
@@ -13,7 +13,63 @@ interface CurrInterface{
     currentNumber: number;
 }
 
-class CurrentMainClass{
+class CurrentMainClass implements CurrInterface{
+    currentString : string;
+    currentNumber : number;
+
+    constructor(){
+        this.currentString = 'CURR STRING';
+        this.currentNumber = 999;
+
+        console.log('currentString:', this.currentString, ' ::: currentNumber:', this.currentNumber);
+    }
 
 }
 
+this.curr_main = new CurrentMainClass();
+//-------------------------------------------------------------------------------------------
+
+
+
+function getArrayLength(x: string[]) {
+    var len = x[0].length;
+    return len;
+}
+
+var names = ['John', 'Dan', 'Aaron', 'Fritz'];
+console.log(getArrayLength(names))
+
+var people = [{name: 'Aaron'}, {name: 'Fritz'}];
+console.log(getArrayLength(people))//--------------------return undefined because array of objects (nor string)
+
+var myFunc2 = (h: number, w: number) => h * w;
+
+console.log('LAMBDA FUNCTION (3*5):', myFunc2(3, 5));
+
+//--------------------------------------------------------------------------------
+
+class GlobalTestClass {
+    private a = 'Private a';
+    public b = 'Public b';
+}
+
+var globclass = new GlobalTestClass();
+console.log('GLOBAL TEST CLASS INSTANCE:', globclass);
+
+
+module Shapes {
+    export class Rectangle {
+        //private privateVar = 'privateVar';
+
+        constructor (
+            public height: number,
+            public width: number) {
+        }
+    }
+    // This works!
+    // var rect1 = new Rectangle(10, 4);
+}
+console.log('SHAPES 1:', Shapes);
+
+var rect2 = Shapes.Rectangle(10, 4);
+console.log('SHAPES 2:', rect2);
